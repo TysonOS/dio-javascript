1747a4c766531dc833c68885f6a5b61b92fe2321
TysonOS <tysonoliveira2013@gmail.com>
2022-01-11 17:28:28 -0300

    atividade pratica sintaxe e operadores

---
+++ sintaxe-operadores/README.md
@@ -0,0 +1,17 @@
+# Sintaxe e Operadores
+
+## Desafio Proposto 👇
+
+### Atividade
+
+- Crie uma função que recebe dois números como parâmetros.
+- Confira se os números são iguais.
+- Confira se a soma dos números é maior que 10 ou menor que 20.
+- Retorne uma string dizendo "Os números `num1` e `num2` não/são iguais. Sua soma é `soma`, que é `maior/menor` que 10 e `maior/menor` que 20".
+
+Exemplo:
+
+```
+Input: 1, 2
+Output: Os números 1 e 2 não são iguais. Sua soma é 3, que é menor que 10 e menor que 20.
+```
---
+++ sintaxe-operadores/comparaNumeros.js
@@ -0,0 +1,43 @@
+function compararNumeros(num1, num2){
+   const frase =  formarFrase(num1, num2);
+   return frase ;
+}
+
+function formarFrase(num1,num2){
+    let iguais;
+    let soma = (num1 + num2)
+    let maiorMenorDez = comparaDez(soma)
+    let maiorMenorVinte = comparaVinte(soma)
+
+    if(num1 == num2){
+        iguais = ''
+    }else if (num1 != num2){
+        iguais = ' não'
+    }
+    
+    return `Os números ${num1} e ${num2}${iguais} são iguais. Sua soma é ${soma}, que é ${maiorMenorDez} 10 e  ${maiorMenorVinte} 20.`
+}
+
+function comparaDez(num){
+    if (num > 10){
+        return 'maior que'
+    }else if (num < 10){
+        return 'menor que'
+    }else{
+        return 'igual a'
+    }
+
+}
+
+function comparaVinte(num){
+    if (num > 20){
+        return 'maior que'
+    }else if (num < 20){
+        return 'menor que'
+    }else{
+        return 'igual a'
+    }
+
+}
+
+console.log(compararNumeros(16,6))
