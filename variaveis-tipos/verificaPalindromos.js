a14929cad4422675a9bd7ae2f87b9431503ece02
TysonOS <tysonoliveira2013@gmail.com>
2022-01-12 15:45:10 -0300

    correções de sintaxe

--- variaveis-tipos/verificaPalindromos.js
+++ variaveis-tipos/verificaPalindromos.js
@@ -1,11 +1,12 @@
 function verificaPalindromo(string){ 
     if (!string || typeof(string)!== 'string'){
-        return "Digite uma frase ou palavra válida"
-    }else {
-        let expressao = string.toLowerCase().replace(/ /gi,"")
-        let expressaoReverse = expressao.split("").reverse("").join("");
-
-        return expressao === expressaoReverse ? "É um palíndromo": "Não é um palidromo"
+        return "Digite uma frase ou palavra válida";
     }
+
+        let expressao = string.toLowerCase().replace(/ /gi,"");
+        let expressaoReverse = expressao.split("").reverse().join("");
+
+        return expressao === expressaoReverse ? "É um palíndromo": "Não é um palidromo";
+    
 }
-console.log(verificaPalindromo("Anotaram a data da maratona"))
+console.log(verificaPalindromo("Roma me tem amor"))
